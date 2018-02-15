86761e24fe03bcb5a93e19f32b66315af8c27308
Tonio Serna <antonelo@gmail.com>
2018-02-15 12:11:36 +0100

    Feature/Add_tweet_display_basic_model | 4: (TDD) Add first green test

--- spec/models/tweet_display_spec.rb
+++ spec/models/tweet_display_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe TweetDisplay, type: :model do
   describe 'given #twitter well configured account with permissions' do
-    it 'has #twitter' do
-      expect(TweetDisplay.twitter).to eq('')
+    it '#twitter has true credentials'do
+      expect(TweetDisplay.twitter.credentials?).to eq(true)
     end
-
   end
 end
