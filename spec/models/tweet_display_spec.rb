a1dc27dc575066a4399465abd0dbd89b6dab007d
Tonio Serna <antonelo@gmail.com>
2018-02-15 12:47:54 +0100

    Feature/Add_tweet_display_basic_model | 6.1: Fix some typos

--- spec/models/tweet_display_spec.rb
+++ spec/models/tweet_display_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe TweetDisplay, type: :model do
-  describe 'given #twitter well configured account with permissions' do
-    it '#twitter has true credentials' do
+  describe 'has #twitter well configured account with permissions' do
+    it 'has true credentials' do
       expect(TweetDisplay.twitter.credentials?).to eq(true)
     end
   end
 
-  describe 'given #twitter well configured account without permissions' do
+  describe 'has #twitter account with errors' do
     let(:nil_token) { nil }
     let(:false_token) { '123456' }
 
-    it '#twitter miss some credential/s' do
+    it 'miss some credential/s' do
       ENV['ACCESS_TOKEN'] = nil_token
       expect { TweetDisplay.twitter.verify_credentials }.to raise_error(Twitter::Error::Forbidden)
     end
 
-    it '#twitter has wrong/unauthorized credentials' do
+    it 'has wrong/unauthorized credentials' do
       ENV['ACCESS_TOKEN'] = false_token
       expect { TweetDisplay.twitter.verify_credentials }.to raise_error(Twitter::Error::Unauthorized)
     end
